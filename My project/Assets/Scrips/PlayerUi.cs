18b57ed381d9c73c87f7e72f7bd887050690a8fc
Mesoneso <Milo.messina@estudiante.imagecampus.edu.ar>
2024-10-17 16:39:46 -0300

    Pause menu finished

--- My project/Assets/Scrips/PlayerUi.cs
+++ My project/Assets/Scrips/PlayerUi.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class PlayerUi : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] FirstPersonLook fpsLook;
+
+    private bool isInPause = false;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            isInPause = !isInPause;
+            
+            pauseMenu.SetActive(isInPause);
+
+            if (isInPause)
+            {
+                Time.timeScale = 0;
+                fpsLook.enabled = false;
+            }
+            else 
+            { 
+                Time.timeScale = 1;
+                fpsLook.enabled = true;
+            }
+        }
     }
 }
