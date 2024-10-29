44ef9b53d976bf14817d50c62b433c4a0d1609c9
Mesoneso <Milo.messina@estudiante.imagecampus.edu.ar>
2024-10-29 16:27:02 -0300

    Edited Triggerers

--- My project/Assets/Scrips/Animator triggerer.cs
+++ My project/Assets/Scrips/Animator triggerer.cs
@@ -19,15 +19,5 @@ public class Animatortriggerer : MonoBehaviour, IInteractable
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
---
+++ My project/Assets/Scrips/Dialogue triggerer.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialoguetriggerer : MonoBehaviour, IInteractable
+{
+    // Start is called before the first frame update
+    public void Interact()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
